4cffec499c11836d2ec8b6a80a881d9c353cb7ff
dl10yr <50223759+dl10yr@users.noreply.github.com>
2020-01-06 18:52:02 +0900

    Update SimpleCard.js

--- src/SimpleCard.js
+++ src/SimpleCard.js
@@ -3,41 +3,34 @@ import ReactDOM from 'react-dom'
 import { translate3d } from './utils'
 
 class Card extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.state = { initialPosition: { x: 0, y: 0 } }
-    this.setInitialPosition = this.setInitialPosition.bind(this)
-  }
-  setInitialPosition () {
-    const card = ReactDOM.findDOMNode(this)
-    const initialPosition = {
-      x: Math.round((this.props.containerSize.x - card.offsetWidth) / 2),
-      y: Math.round((this.props.containerSize.y - card.offsetHeight) / 2)
-    }
-    this.setState({ initialPosition })
   }
 
-  componentDidMount () {
-    this.setInitialPosition()
-    window.addEventListener('resize', this.setInitialPosition)
-  }
-
-  componentWillUnmount () {
-    window.removeEventListener('resize', this.setInitialPosition)
-  }
-
-  render () {
+  render() {
     const { initialPosition: { x, y } } = this.state
-    const { className = 'inactive' } = this.props
+    const { className = 'inactive', overlay } = this.props
     var style = {
       ...translate3d(x, y),
       zIndex: this.props.index,
-      ...this.props.style
+      ...this.props.style,
     }
 
     return (
-      <div style={style} className={`card ${className}`}>
+      <div style={{ ...style }} className={`card ${className}`}>
         {this.props.children}
+        {overlay &&
+          <div style={{
+            position: 'absolute',
+            top: 0, right: 0, bottom: 0, left: 0,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            {overlay}
+          </div>
+        }
       </div>
     )
   }
