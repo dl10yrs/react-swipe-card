24d9bf2b7a355b033ce6536861b4bf71e10ae67d
dl10yr <50223759+dl10yr@users.noreply.github.com>
2020-01-06 19:01:17 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import Cards from './Cards'
 
-export Card from './CardSwitcher'
+export { default as Card } from './CardSwitcher'
+
+Number.prototype.map = function ({ inputRange: [iMin, iMax], outputRange: [oMin, oMax], clamp = false }) {
+    let finalValue = (this.valueOf() - iMin) / (iMax - iMin) * (oMax - oMin) + oMin;
+    if (clamp) {
+        if (finalValue > oMax) {
+            return oMax
+        } else if (finalValue < oMin) {
+            return oMin
+        }
+    }
+    return finalValue;
+}
 
 export default Cards
